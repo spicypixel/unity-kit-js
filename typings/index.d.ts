ced82074dc3b16c3bed930e3d0f71118daeeb241
Aaron Oneal <aaron.oneal@spicypixel.com>
2016-05-29 11:24:00 -0700

    Save typings

---
+++ typings/index.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="globals/es6-promise/index.d.ts" />
+/// <reference path="globals/fs-extra/index.d.ts" />
+/// <reference path="globals/mocha/index.d.ts" />
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="globals/path-exists/index.d.ts" />
+/// <reference path="modules/bluebird/index.d.ts" />
+/// <reference path="modules/chai/index.d.ts" />
