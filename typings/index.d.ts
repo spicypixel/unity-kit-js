791c941389266588efc4506d1efe7e15c5f3ad17
Aaron Oneal <aaron.oneal@spicypixel.com>
2016-05-29 15:07:31 -0700

    Update packages

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -2,6 +2,6 @@
 /// <reference path="globals/fs-extra/index.d.ts" />
 /// <reference path="globals/mocha/index.d.ts" />
 /// <reference path="globals/node/index.d.ts" />
-/// <reference path="globals/path-exists/index.d.ts" />
 /// <reference path="modules/bluebird/index.d.ts" />
+/// <reference path="modules/chai-as-promised/index.d.ts" />
 /// <reference path="modules/chai/index.d.ts" />
