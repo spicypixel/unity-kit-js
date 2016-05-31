3595d3ef0b40be1b036a50ac4c209e23a3fb455a
Aaron Oneal <aaron.oneal@spicypixel.com>
2016-05-30 20:28:30 -0700

    Simplify gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,15 @@
-// See: http://www.smashingmagazine.com/2014/06/11/building-with-gulp/
-
-// Configure in WebStorm an output filter: $FILE_PATH$[ \t]*[:;,\[\(\{<]$LINE$(?:[:;,\.]$COLUMN$)?.*
-
 "use strict";
 
-// Gulp basics
-var gulp = require("gulp");
-var gutil = require("gulp-util");
-var gulpif = require("gulp-if");
-
-// Streams and process
-var eventStream = require("event-stream");
-var argv = require("yargs").argv;
-var sourcemaps = require("gulp-sourcemaps");
-let fs = require("fs-extra");
-
 // Tools
+var gulp = require("gulp");
 var ts = require("gulp-typescript");
 var tslint = require("gulp-tslint");
 var mocha = require("gulp-mocha");
 
-// Command line option:
-//  --fatal=[warning|error|off]
-var fatalLevel = require("yargs").argv.fatal;
-
-var ERROR_LEVELS = ["error", "warning"];
-
-function isFatal(level) {
-  return ERROR_LEVELS.indexOf(level) <= ERROR_LEVELS.indexOf(fatalLevel || "error");
-}
-
-function handleError(level, error) {
-  // gutil.log(error.message);
-  if (isFatal(level)) {
-    process.exit(1);
-  }
-  else {
-    gutil.log(error.message);
-  }
-}
-
-function onError(error) { handleError.call(this, "error", error);}
-function onWarning(error) { handleError.call(this, "warning", error);}
+// Streams and process
+var eventStream = require("event-stream");
+var sourcemaps = require("gulp-sourcemaps");
+let fs = require("fs-extra");
 
 gulp.on("error", function(err) {
   console.log(err);
@@ -57,39 +25,29 @@ gulp.task("tsc", function() {
     .pipe(tslint.report("verbose"));
 
   var tscResult = tsProject.src()
-    .pipe(gulpif(!argv.release, sourcemaps.init({loadMaps: true}))) // This means sourcemaps will be generated
-    .pipe(ts(tsProject))
-    .on("error", onError);
+    .pipe(sourcemaps.init({ loadMaps: true }))
+    .pipe(ts(tsProject));
 
   var dtsResult = tscResult.dts
-    .pipe(gulp.dest("./"))
-    .on("error", onError);
-    
+    .pipe(gulp.dest("./"));
+
   var jsResult = tscResult.js
-    .pipe(gulpif(!argv.release, sourcemaps.write("./", {includeContent: true, sourceRoot: "src/lib/"}))) // source files under this root
-    .pipe(gulp.dest("./"))
-    .on("error", onError);
-  
+    .pipe(sourcemaps.write("./", { includeContent: true, sourceRoot: "../src" }))
+    .pipe(gulp.dest("./"));
+
   return eventStream.merge(lintResult, jsResult, dtsResult);
 });
 
-// Build
-gulp.task("build", ["tsc"], function() {
-});
+gulp.task("build", ["tsc"]);
 
-// Install
-gulp.task("install", ["build"], function() {
-});
-
-gulp.task("default", ["test"], function() {
-});
+gulp.task("default", ["test"]);
 
 gulp.task("test", ["build"], function() {
-  return gulp.src("./test/**/*.js", {read: false})
+  return gulp.src("./test/**/*.js", { read: false })
 		.pipe(mocha());
 });
 
 gulp.task("clean", function() {
-  fs.remove("lib");
-  fs.remove("test");
+  fs.removeSync("lib");
+  fs.removeSync("test");
 });
