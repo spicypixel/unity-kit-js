7ef9127d86f57b2209285b1672ea3a626eaed734
Aaron Oneal <aaron.oneal@spicypixel.com>
2017-08-23 18:19:03 -0700

    Update exports

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,3 +1,5 @@
 export { default as UnityEditor } from "./unity-editor";
 export { default as UnityProject } from "./unity-project";
 export { default as UnityModuleManager } from "./unity-module-manager";
+export { default as UnityModuleLibraryReference } from "./unity-module-library-reference";
+export { default as UnityModuleReference } from "./unity-module-reference";
